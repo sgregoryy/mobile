1f38299c4b4d0381ade6bf35a21409f77bd3d812
Ramis Mukhlisov <ramis5454668@gmail.com>
2024-12-19 21:47:33 +0300

    fix: main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 import 'screens/home_screen.dart';
 import 'models/word_card.dart';
 
 void main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
-    
+
     if (kIsWeb) {
-      // Специальная инициализация для веб
       await Hive.initFlutter('hive_db');
     } else {
-      await Hive.initFlutter();
+      // Получаем путь к директории приложения
+      final appDocumentDir = await getApplicationDocumentsDirectory();
+      await Hive.initFlutter(appDocumentDir.path);
     }
-    
+
     if (!Hive.isAdapterRegistered(0)) {
       Hive.registerAdapter(WordCardAdapter());
     }
-    
+
     await Hive.openBox<WordCard>('wordCards',
-      path: kIsWeb ? null : 'wordcards_db',
+      // Убираем явное указание пути
       compactionStrategy: (entries, deletedEntries) => deletedEntries > 20,
     );
-    
+
     runApp(const FlashcardsApp());
-    
+
   } catch (e, stackTrace) {
     if (kDebugMode) {
       print('Error initializing app: $e');
@@ -33,6 +35,7 @@ void main() async {
     }
   }
 }
+
 
 class FlashcardsApp extends StatelessWidget {
   const FlashcardsApp({super.key});
